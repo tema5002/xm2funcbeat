cdd3f79666067644466b0f6f49a07de0a39f8431
tema5002 <25244474-xtema5002x@users.noreply.replit.com>
2024-06-24 18:21:21 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <fstream>
+using namespace std;
+
+ifstream F("BFIce.xm", ios::binary);
+
+// ok wait uhhhh
+// no i said that you would do it open git tab
+vector<uint8_t> read_bytes(int num_bytes) {
+    vector<uint8_t> buffer(num_bytes);
+    F.read(reinterpret_cast<char*>(buffer.data()), num_bytes);
+    return buffer;
+}
+
+
+int main() {
+    auto wuggyvector = read_bytes(100);
+    for (auto a : wuggyvector)
+        cout << a;
+    cout << std::endl;
+    cin.get();
+    return 0;
+}
