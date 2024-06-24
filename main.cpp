b0db524d1822cadf2025149be3bd0f2d2d9d1075
d <153951662+dtplsongithub@users.noreply.github.com>
2024-06-24 18:43:01 +0000

    moo

--- main.cpp
+++ main.cpp
@@ -4,20 +4,38 @@ using namespace std;
 
 ifstream F("BFIce.xm", ios::binary);
 
-// ok wait uhhhh
-// no i said that you would do it open git tab
-vector<uint8_t> read_bytes(int num_bytes) {
-    vector<uint8_t> buffer(num_bytes);
-    F.read(reinterpret_cast<char*>(buffer.data()), num_bytes);
-    return buffer;
+/*
+typing typingers typi
+why was blud observing me
+wow it did Nothing!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+did you make the webmook <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+did you make the webmook 
+yeah did you push anything
+not yet
+*/
+
+char read_byte() {
+    char byte;
+    F >> byte;
+    return byte;
 }
 
+vector<char> read_bytes(int b) {
+    vector<char> bytes;
+    for (int i = 0; i < b; i++) {
+        bytes.push_back(read_byte());
+    }
+    return bytes;
+}
 
 int main() {
-    auto wuggyvector = read_bytes(100);
-    for (auto a : wuggyvector)
-        cout << a;
-    cout << std::endl;
-    cin.get();
+  /*
+    vector<char> bytes = read_bytes(100);
+    for (int i = 0; i < 100; i++) {
+        cout << bytes[i] << ' ';
+    }
+    cout << endl;
+  */
+    cout << read_byte();
     return 0;
 }
